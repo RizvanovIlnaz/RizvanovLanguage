3d5952693e60bfab011a1cf3aeab4e6ebdf17026
rizil <riz.ilnaz2005@gmail.com>
2024-04-02 17:46:08 +0530

    23

--- ClientPage.xaml.cs
+++ ClientPage.xaml.cs
@@ -20,12 +20,189 @@ namespace RizvanovLanguage
     /// </summary>
     public partial class ClientPage : Page
     {
+
+        int CountInPage = 10;
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Client> CurrentPageList = new List<Client>();
+        List<Client> TableList;
         public ClientPage()
         {
             InitializeComponent();
             List<Client> currentClients = RizvanovLanguageEntities.GetContext().Client.ToList();
 
             ClientListView.ItemsSource = currentClients;
+
+            strCount.SelectedIndex = 0;
+            TBAllRecords.Text = RizvanovLanguageEntities.GetContext().Client.ToList().Count().ToString();
+            Update();
+        }
+
+        public void Update()
+        {
+            var currentClient = RizvanovLanguageEntities.GetContext().Client.ToList();
+
+            TBAllRecords.Text = RizvanovLanguageEntities.GetContext().Client.ToList().Count().ToString();
+            TBCount.Text = currentClient.Count().ToString();
+
+            ClientListView.ItemsSource = currentClient;
+
+            TableList = currentClient;
+
+            if (strCount.SelectedIndex == 0)
+            {
+                CountInPage = 10;
+            }
+            else if (strCount.SelectedIndex == 1)
+            {
+                CountInPage = 50;
+            }
+            else if (strCount.SelectedIndex == 2)
+            {
+                CountInPage = 200;
+            }
+            else if (strCount.SelectedIndex == 3)
+            {
+                CountInPage = 0;
+            }
+
+
+            ChangePage(0, 0);
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountInPage != 0)
+            {
+                if (CountRecords % CountInPage > 0)
+                {
+                    CountPage = CountRecords / CountInPage + 1;
+                }
+                else
+                {
+                    CountPage = CountRecords / CountInPage;
+                }
+
+                Boolean Ifupdate = true;
+
+                int min;
+
+                if (selectedPage.HasValue)
+                {
+                    if (selectedPage >= 0 && selectedPage <= CountPage)
+                    {
+                        CurrentPage = (int)selectedPage;
+                        min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                        for (int i = CurrentPage * CountInPage; i < min; i++)
+                        {
+                            CurrentPageList.Add(TableList[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    switch (direction)
+                    {
+                        case 1:
+                            if (CurrentPage > 0)
+                            {
+                                CurrentPage--;
+                                min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                                for (int i = CurrentPage * CountInPage; i < min; i++)
+                                {
+                                    CurrentPageList.Add(TableList[i]);
+                                }
+                            }
+                            else
+                            {
+                                Ifupdate = false;
+                            }
+                            break;
+                        case 2:
+                            if (CurrentPage < CountPage - 1)
+                            {
+                                CurrentPage++;
+                                min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                                for (int i = CurrentPage * CountInPage; i < min; i++)
+                                {
+                                    CurrentPageList.Add(TableList[i]);
+                                }
+                            }
+                            else
+                            {
+                                Ifupdate = false;
+                            }
+                            break;
+                    }
+                }
+                if (Ifupdate)
+                {
+                    PageListBox.Items.Clear();
+                    for (int i = 1; i <= CountPage; i++)
+                    {
+                        PageListBox.Items.Add(i);
+                    }
+                    PageListBox.SelectedIndex = CurrentPage;
+
+                    min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                    TBCount.Text = min.ToString();
+                    TBAllRecords.Text = CountRecords.ToString();
+
+                    ClientListView.ItemsSource = CurrentPageList;
+
+                    ClientListView.Items.Refresh();
+                }
+            }
+            else
+            {
+                PageListBox.Items.Clear();
+                PageListBox.Items.Add(1);
+            }
+        }
+
+
+        private void strCount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var currentClient = (sender as Button).DataContext as Client;
+
+            if (currentClient.VisitCount == 0)
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    RizvanovLanguageEntities.GetContext().Client.Remove(currentClient);
+                   RizvanovLanguageEntities.GetContext().SaveChanges();
+                    ClientListView.ItemsSource = RizvanovLanguageEntities.GetContext().Client.ToList();
+                    Update();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Невозможно выполнить удаление, так как клиент посещал школу!");
+            }
         }
     }
 }
